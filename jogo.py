fae80473bdd6ae3c1390f3a8ea5eefed0637e521
weverton henrique <wevertonwhm18@outlook.com>
2025-02-05 21:48:51 -0300

    .

---
+++ jogo.py
@@ -0,0 +1,19 @@
+import random
+
+#Lista para armazenar as pontuações
+pontuacoes = []
+
+print("Bem vindo ao jogo de Adivinhação")
+
+while True:
+    numero_secreto = random.randint(1, 100)
+    tentativas_restantes = 7
+    tentativas = 0
+
+    print("\nEu pensei em um número entre 1 e 100.")
+    print("Você tem 7 tentativas para adivinhar.")
+
+    
+
+
+
